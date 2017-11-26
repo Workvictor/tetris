e7891dcb0c09a7c77676cc16004bf77a54724766
workvictor <workvictor@yandex.ru>
2017-11-26 22:26:01 +0300

    feat add render functions

--- src/components/engine/Grid.js
+++ src/components/engine/Grid.js
@@ -1,10 +1,42 @@
+import { CanvasApi } from './index';
 export class Grid {
-  constructor({ width, height }) {
+  constructor({ width, height, cellSize }) {
     this.data = Array(height).fill(Array(width).fill(0));
+    this.canvas = new CanvasApi({
+      aspectRatio: 10 / 20,
+      width: cellSize * width
+    });
+    this.cellSize = cellSize;
+    this.sprite = this.canvas.create.canvas({ width:width*cellSize, height:height*cellSize });
+    this.linesDataSprites = Array(height).fill(this.drawEmptyLine);
   }
 
+  get drawEmptyLine(){
+    return this.canvas.create.canvas({ 
+      width: this.data[0].length*this.cellSize, 
+      height: this.cellSize
+    });
+  }
+
+  get addEmptyLine(){
+    return Array(this.data[0].length).fill(0);
+  }
+  
   get randomX() {
-    return Math.random() * this.data[0].length - 1;
+    return Math.floor(Math.random() * this.data[0].length - 1);
+  }
+
+  
+
+  sliceSprite=()=>{
+    const getSpriteLine = y => {
+      const width = this.data[0].length * this.cellSize;
+      const height = this.cellSize;
+      const buffer = this.canvas.create.canvas({ width, height });
+      buffer.ctx.drawImage(this.sprite, 0, y * this.cellSize, width, height, 0, 0, width, height);
+      return buffer;
+    };
+    this.linesDataSprites = this.data.map((row, indexY) => getSpriteLine(indexY))
   }
 
   row(y) {
@@ -16,17 +48,20 @@ export class Grid {
       return this.data.map((row, index) => row[x]);
   }
 
-  isShapeCollide = ({ shape, x, y }) => 
-    shape.some((row, indexY) =>
-      row.some((elem, indexX) => {
-        return elem !==0 && this.data[y + indexY][x + indexX] === -1
-      })
-    );
+  renderLinesData=()=>{
+    const width = this.data[0].length * this.cellSize;
+    const height = this.data.length * this.cellSize;
+    let buffer = this.canvas.create.canvas({ width, height });
+    this.linesDataSprites.forEach((elem, y) => {
+      buffer = this.canvas.compose.normal(buffer, elem, 0, y*this.cellSize);
+    })
+    this.sprite = buffer;
+  }
 
-  addShapeLanded = ({ shape, x, y }) => {
+  addShapeLanded = ({ shape, x, y, sprite }) => {
     shape.forEach((row, indexY) =>
       row.forEach((elem, indexX) => {
-        if (y + indexY >= 0) {
+        if (y + indexY >= 0 && elem===1) {
           this.data = [
             ...this.data.slice(0, y + indexY),
             [
@@ -39,23 +74,29 @@ export class Grid {
         }
       })
     );
+    this.sprite = this.canvas.compose.normal(this.sprite, sprite, x*this.cellSize, y*this.cellSize);
+    this.sliceSprite();
+    this.checkLinesFullnes();
   };
 
-  addShape = ({ shape, x, y }) => {
-    shape.forEach((row, indexY) =>
-      row.forEach((elem, indexX) => {
-        if (y + indexY >= 0) {
-          this.data = [
-            ...this.data.slice(0, y + indexY),
-            [
-              ...this.data[y + indexY].slice(0, x + indexX),
-              elem,
-              ...this.data[y + indexY].slice(x + indexX + 1)
-            ],
-            ...this.data.slice(y + indexY + 1)
-          ];
-        }
-      })
-    );
-  };
+  removeLine=(lineIndex)=>{
+    this.data = [
+      this.addEmptyLine,
+      ...this.data.slice(0, lineIndex),
+      ...this.data.slice(lineIndex + 1)
+    ];
+    this.linesDataSprites = [
+      this.drawEmptyLine,
+      ...this.linesDataSprites.slice(0, lineIndex),
+      ...this.linesDataSprites.slice(lineIndex + 1)
+    ];
+    this.renderLinesData();
+  }
+
+  checkLinesFullnes=()=>{
+    this.data.forEach((row, y)=>{
+      row.every(elem=>elem === -1)  && this.removeLine(y)
+    })
+  }
+
 }
