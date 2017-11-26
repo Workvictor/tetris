e189e14d145e3a34e9c0935f6a2062c0df2d0b7a
workvictor <workvictor@yandex.ru>
2017-11-26 12:17:51 +0300

    feat provide the store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux'
 import { App } from './App';
+import { STORE } from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={STORE}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
