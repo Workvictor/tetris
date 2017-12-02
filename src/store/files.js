d759bc396869d54cc3069e1d71f5f0bfa5048cd6
workvictor <workvictor@yandex.ru>
2017-12-02 18:47:07 +0300

    refactor remove unused files

--- src/store/files.js
+++ src/store/files.js
@@ -1,81 +1,72 @@
-import { TYPES } from "./actions";
+import { TYPES } from './actions';
 
 export const files = (
-  state = [    
+  state = [
     {
       name: 'awake10_megaWall.mp3',
       role: 'MUSIC_title',
       content: 'audio',
-      tag: 'music',
+      tag: 'music'
     },
     {
       name: 'Yerzmyey-The_Lady_of_Mars.mp3',
       role: 'MUSIC_gameTheme',
       content: 'audio',
-      tag: 'music',
+      tag: 'music'
     },
     {
       name: 'generic_sounds/flagreturn.wav',
       role: 'SFX_remove',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/antimaterhit.wav',
       role: 'SFX_drop',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/Warm_Digital_Accept_Button_HP.wav',
       role: 'SFX_move',
-      content: 'audio',
+      content: 'audio'
     },
     {
-      name: 'generic_sounds/little_robot_sound_factory_Jingle_Achievement_00.mp3',
+      name:
+        'generic_sounds/little_robot_sound_factory_Jingle_Achievement_00.mp3',
       role: 'SFX_combo',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/game_hidemenu.wav',
       role: 'SFX_gameOver',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/explosion4.wav',
       role: 'SFX_landing',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/itemback.wav',
       role: 'SFX_hover',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/itempick1.wav',
       role: 'SFX_pick',
-      content: 'audio',
+      content: 'audio'
     },
     {
       name: 'generic_sounds/tick.wav',
       role: 'SFX_tick',
-      content: 'audio',
-    },
-    {
-      name: 'spaceStars.png',
-      role: 'img_stars',
-      content: 'img',
-    },
-    {
-      name: 'milky_way.jpg',
-      role: 'img_bg',
-      content: 'img',
+      content: 'audio'
     }
   ],
   { type, payload }
-) => 
+) =>
   type === TYPES.UPDATE_FILE_INFO
-  ? [
-      ...state.slice(0, state.findIndex(elem=>elem.name===payload.name)),
-      {...state.find(elem=>elem.name===payload.name), ...payload},
-      ...state.slice(state.findIndex(elem=>elem.name===payload.name)+1)
-    ]
-  : state;
+    ? [
+        ...state.slice(0, state.findIndex(elem => elem.name === payload.name)),
+        { ...state.find(elem => elem.name === payload.name), ...payload },
+        ...state.slice(state.findIndex(elem => elem.name === payload.name) + 1)
+      ]
+    : state;
