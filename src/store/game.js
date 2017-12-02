ac3f39db8bc4bdf2c7d60b3ed1efaa4a237ad5da
workvictor <workvictor@yandex.ru>
2017-12-02 18:44:56 +0300

    feat add new reducer

---
+++ src/store/game.js
@@ -0,0 +1,31 @@
+import { TYPES } from './actions';
+export const game = (
+  state = {
+    cellSize: 64,
+    gridAspectRatio: 1/1,
+    displayDefaults: {
+      width: 640,
+      aspectRatio: 16/9
+    },
+    score: 0,
+    pause: false,
+    newGameStarts: false,
+    scoreStack: [],
+    showScore: 0,
+  },
+  { type, payload }
+) =>
+    type === TYPES.UPDATE_GAME
+  ? { ...state, ...payload}
+  : type === TYPES.UPDATE_SCORE_STACK
+  ? { ...state, scoreStack:[
+      ...state.scoreStack,
+      ...payload.scoreStack
+      ]
+    }
+  : type === TYPES.UPDATE_SHOW_SCORE
+  ? { ...state, ...payload, scoreStack:[
+      ...state.scoreStack.slice(0,0),
+      ...state.scoreStack.slice(1),
+    ]}
+  : state;
