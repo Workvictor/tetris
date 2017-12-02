f5c33ec90532cf85f214a4bb0c3bac511ebc7e85
workvictor <workvictor@yandex.ru>
2017-12-02 18:41:53 +0300

    feat add new functional

--- src/components/engine/Grid.js
+++ src/components/engine/Grid.js
@@ -1,32 +1,67 @@
-import { CanvasApi } from './index';
+import { CanvasApi, STORE, ACTIONS } from './index';
 export class Grid {
-  constructor({ width, height, cellSize }) {
-    this.data = Array(height).fill(Array(width).fill(0));
-    this.canvas = new CanvasApi({
-      aspectRatio: 10 / 20,
-      width: cellSize * width
-    });
+  constructor({ width, height, cellSize, offset=0 }) {
+    this.offset = offset;
     this.cellSize = cellSize;
-    this.sprite = this.canvas.create.canvas({ width:width*cellSize, height:height*cellSize });
-    this.linesDataSprites = Array(height).fill(this.drawEmptyLine);
-  }
+    this.width =  width;
+    this.height =  height + offset;
 
-  get drawEmptyLine(){
-    return this.canvas.create.canvas({ 
-      width: this.data[0].length*this.cellSize, 
-      height: this.cellSize
+    this.canvas = new CanvasApi({
+      aspectRatio: this.width / this.height,
+      width: this.cellSize * this.width
     });
+
+    this.data = Array(this.height).fill(Array(this.width).fill(0));
+
+    this.sprite = this.canvas.create.canvas({ width: this.width*cellSize, height: this.height*cellSize });
+    this.linesDataSprites = Array(height).fill(null);
   }
 
-  get addEmptyLine(){
+  updateGameScore=(score)=>{
+    const deltaScore = score - this.gameScore;
+    const incrementStep = Math.floor(deltaScore/this.store.game.incrementCount);
+    const scoreStack = Array(this.store.game.incrementCount).fill(incrementStep);
+    const scoreStackSum = scoreStack.reduce((sum, val)=>sum+val, this.gameScore);
+    scoreStackSum < score &&
+    scoreStack.push( score - scoreStackSum );
+
+    STORE.dispatch(ACTIONS.updateGame({ score }))
+    STORE.dispatch(ACTIONS.updateScoreStack({ scoreStack }))
+  };
+
+  isNotValid=(y=0,x=0)=>this.data[y][x] !== 0;
+
+  reset =()=>{
+    this.data = Array(this.height).fill(Array(this.width).fill(0));
+    this.sprite = this.canvas.create.canvas({ width: this.width*this.cellSize, height: this.height*this.cellSize });
+  }
+
+  get action(){
+    const updateLinesCount =(linesCount=0, dropDelay=0)=> STORE.dispatch(ACTIONS.updateGame({
+      linesCount, dropDelay
+    }));
+    return{
+      updateLinesCount,
+    }
+  }
+
+  get store(){
+    return STORE.getState();
+  }
+
+  get gameScore(){
+    return STORE.getState().game.score;
+  }
+
+  get emptyLine(){
     return Array(this.data[0].length).fill(0);
   }
-  
-  get randomX() {
-    return Math.floor(Math.random() * this.data[0].length - 1);
-  }
 
-  
+  randomX=(
+    {shape},
+    min=0,
+    max=this.data[0].length - 1 - shape[0].length
+  )=> Math.floor(min + (Math.random()*max-min));
 
   sliceSprite=()=>{
     const getSpriteLine = y => {
@@ -53,7 +88,7 @@ export class Grid {
     const height = this.data.length * this.cellSize;
     let buffer = this.canvas.create.canvas({ width, height });
     this.linesDataSprites.forEach((elem, y) => {
-      buffer = this.canvas.compose.normal(buffer, elem, 0, y*this.cellSize);
+      if(elem) buffer = this.canvas.compose.normal(buffer, elem, 0, y*this.cellSize);
     })
     this.sprite = buffer;
   }
@@ -76,27 +111,42 @@ export class Grid {
     );
     this.sprite = this.canvas.compose.normal(this.sprite, sprite, x*this.cellSize, y*this.cellSize);
     this.sliceSprite();
-    this.checkLinesFullnes();
+    this.checkLinesFullness();
   };
 
-  removeLine=(lineIndex)=>{
+  removeLine=(lineIndex, multiplierIndex)=>{
+    const { lineScore, lineMultiplier } = this.store.game;
     this.data = [
-      this.addEmptyLine,
+      this.emptyLine,
       ...this.data.slice(0, lineIndex),
       ...this.data.slice(lineIndex + 1)
     ];
     this.linesDataSprites = [
-      this.drawEmptyLine,
+      null,
       ...this.linesDataSprites.slice(0, lineIndex),
       ...this.linesDataSprites.slice(lineIndex + 1)
     ];
     this.renderLinesData();
+    multiplierIndex === 3 && this.store.audioController.playSoundByROLE('SFX_combo');
+    const score = multiplierIndex > 0 ? lineScore*lineMultiplier*multiplierIndex : lineScore;
+    this.updateGameScore(Math.floor(this.gameScore + score));
   }
 
-  checkLinesFullnes=()=>{
-    this.data.forEach((row, y)=>{
-      row.every(elem=>elem === -1)  && this.removeLine(y)
-    })
+  removeLinesStack=(lines)=>{
+    const linesCount = this.store.game.linesCount + lines.length;
+    const dropDelay = this.store.game.dropDelay - lines.length;
+    this.action.updateLinesCount(linesCount, dropDelay)
+    lines.forEach(this.removeLine)
+    this.store.audioController.playSoundByROLE('SFX_remove');
+  }
+
+  checkLinesFullness=()=>{
+    const lines = this.data.map(
+      (row, y)=>({row, y})).filter(
+        ({row,y})=> row.every(
+          elem=>elem===-1)).map(
+            elem=>elem.y);
+    lines.length > 0 && this.removeLinesStack(lines);
   }
 
 }
